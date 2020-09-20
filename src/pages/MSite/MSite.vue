55b50c5a830cf0462211315291d3a2e6655298f9
napeng2 <18518116088@163.com>
2020-09-20 18:46:47 +0800

    new

--- src/pages/MSite/MSite.vue
+++ src/pages/MSite/MSite.vue
@@ -1,15 +1,76 @@
 <!--  -->
 <template>
-  <header-top :title="'北京'">
-    <router-link slot="left" to="/search" class="header_search">
+  <div class="msite">
+    <header-top :title="'北京'">
+      <router-link slot="left" to="/search" class="header_search">
         <i class="iconfont icon-sousuo"></i>
-    </router-link>
-    <router-link slot="right" to="#" class="header_login">登陆|注册</router-link>
-  </header-top>
+      </router-link>
+      <router-link slot="right" to="#" class="header_login">登陆|注册</router-link>
+    </header-top>
+    <!-- 首页导航 -->
+    <nav class="msite_nav border-1px">
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/1.jpg" />
+              </div>
+              <span>甜品饮品</span>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/1.jpg" />
+              </div>
+              <span>甜品饮品</span>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/1.jpg" />
+              </div>
+              <span>甜品饮品</span>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/1.jpg" />
+              </div>
+              <span>甜品饮品</span>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/1.jpg" />
+              </div>
+              <span>甜品饮品</span>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/1.jpg" />
+              </div>
+              <span>甜品饮品</span>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/1.jpg" />
+              </div>
+              <span>甜品饮品</span>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container">
+                <img src="./images/nav/1.jpg" />
+              </div>
+              <span>甜品饮品</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <ShopList></ShopList>
+  </div>
 </template>
 
 <script>
 import HeaderTop from "../../components/HeaderTop/HeaderTop";
+import ShopList from "../../components/ShopList/ShopList";
 export default {
   data() {
     return {};
@@ -17,9 +78,68 @@ export default {
 
   components: {
     HeaderTop,
+    ShopList,
   },
 };
 </script>
 <style lang='stylus' scoped>
-
+ @import "../../common/stylus/mixins.styl"
+  .msite  //首页
+    width 100%
+    // .miste-content-wrapper
+    //   position fixed
+    //   top: 45px
+    //   bottom: 46px
+    //   width: 100%
+   .msite_nav
+        bottom-border-1px(#e4e4e4)
+        margin-top 50px
+        height 200px
+        background #fff
+        .swiper-container
+          background-color #fff
+          width 100%
+          height 100%
+          .swiper-wrapper
+            width 100%
+            height 100%
+            .swiper-slide
+              display flex
+              justify-content center
+              align-items flex-start
+              flex-wrap wrap
+              .link_to_food
+                width 25%
+                .food_container
+                  display block
+                  width 100%
+                  text-align center
+                  padding-bottom 10px
+                  font-size 0
+                  img
+                    display inline-block
+                    width 50px
+                    height 50px
+                span
+                  display block
+                  width 100%
+                  text-align center
+                  font-size 13px
+                  color #666
+          .swiper-pagination
+            >span.swiper-pagination-bullet-active
+              background #02a774
+      .msite_shop_list
+        top-border-1px(#e4e4e4)
+        margin-top 10px
+        background #fff
+        .shop_header
+          padding 10px 10px 0
+          .shop_icon
+            margin-left 5px
+            color #999
+          .shop_header_title
+            color #999
+            font-size 14px
+            line-height 20px
 </style>
